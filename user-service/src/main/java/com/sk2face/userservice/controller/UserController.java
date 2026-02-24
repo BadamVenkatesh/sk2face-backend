4f13476cc769c92ce4d89b32495eb5a400d649b2
BadamVenkatesh <venkateshbadamdurga@gmail.com>
2026-02-24 23:11:03 +0530

    fix(user-service): correct typo in service and update ResponseBuilder naming

--- user-service/src/main/java/com/sk2face/userservice/controller/UserController.java
+++ user-service/src/main/java/com/sk2face/userservice/controller/UserController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
     private final ResponseBuilder responseBuilder;
 
